18d3e4c80e524aa0ee8150f61aae4cb0b46b6347
IQlikeinBREAD <filaret2003@gmail.com>
2023-10-25 19:24:06 +0200

    Create Program.cs

---
+++ IoTProjectFilaretVasiliev/IoTProjectFilaretVasiliev/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace IoTProjectFilaretVasiliev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("gfgfgd000");
+            Console.WriteLine("gfgfgd000");
+        }
+    }
+}
