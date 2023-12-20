5c1687fa7bdae8b77c7db281f2f5b3e72de7a94b
IQlikeinBREAD <118021713+IQlikeinBREAD@users.noreply.github.com>
2023-12-20 15:20:30 +0100

    Program.cs

--- IoTProjectFilaretVasiliev/IoTProjectFilaretVasiliev/OpcAgent/OpcAgent/Program.cs
+++ IoTProjectFilaretVasiliev/IoTProjectFilaretVasiliev/OpcAgent/OpcAgent/Program.cs
@@ -1,31 +1,61 @@
-﻿using Opc.UaFx;
+using Opc.UaFx;
 using Opc.UaFx.Client;
 
 using (var client = new OpcClient("opc.tcp://localhost:4840/"))
 {
     client.Connect();
 
-    OpcReadNode[] commands = new OpcReadNode[] {
-    new OpcReadNode("ns=2;s=Device 1/ProductionStatus", OpcAttribute.DisplayName),
-    new OpcReadNode("ns=2;s=Device 1/ProductionStatus"),
-    new OpcReadNode("ns=2;s=Device 1/ProductionRate", OpcAttribute.DisplayName),
-    new OpcReadNode("ns=2;s=Device 1/ProductionRate"),
-    new OpcReadNode("ns=2;s=Device 1/WorkorderId", OpcAttribute.DisplayName),
-    new OpcReadNode("ns=2;s=Device 1/WorkorderId"),
-    new OpcReadNode("ns=2;s=Device 1/Temperature", OpcAttribute.DisplayName),
-    new OpcReadNode("ns=2;s=Device 1/Temperature"),
-    new OpcReadNode("ns=2;s=Device 1/GoodCount", OpcAttribute.DisplayName),
-    new OpcReadNode("ns=2;s=Device 1/GoodCount"),
-    new OpcReadNode("ns=2;s=Device 1/BadCount", OpcAttribute.DisplayName),
-    new OpcReadNode("ns=2;s=Device 1/BadCount"),
-    new OpcReadNode("ns=2;s=Device 1/DeviceError", OpcAttribute.DisplayName),
-    new OpcReadNode("ns=2;s=Device 1/DeviceError"),
-};
+    var endpoints = client.EnumerateEndpoints();
 
-    IEnumerable<OpcValue> job = client.ReadNodes(commands);
+    int deviceCount = 0;
+    foreach (var endpoint in endpoints)
+    {
+        if (endpoint.NamespaceUri == "ns=2" && endpoint.BrowsePath.Contains("/Device"))
+        {
+            deviceCount++;
+        }
+    }
 
+    Console.WriteLine("Number of devices: {0}", deviceCount);
+
+    // Prepare read nodes for all devices
+    var readNodes = new OpcReadNode[deviceCount];
+    int index = 0;
+    foreach (var endpoint in endpoints)
+    {
+        if (endpoint.NamespaceUri == "ns=2" && endpoint.BrowsePath.Contains("/Device"))
+        {
+            var deviceId = endpoint.BrowsePath.Substring(5);
+
+            // Read node for "ProductionStatus"
+            readNodes[index] = new OpcReadNode("ns=2;s={0}/ProductionStatus", deviceId);
+
+            // Read node for "ProductionRate"
+            readNodes[++index] = new OpcReadNode("ns=2;s={0}/ProductionRate", deviceId);
+
+            // Read node for "WorkorderId"
+            readNodes[++index] = new OpcReadNode("ns=2;s={0}/WorkorderId", deviceId);
+
+            // Read node for "Temperature"
+            readNodes[++index] = new OpcReadNode("ns=2;s={0}/Temperature", deviceId);
+
+            // Read node for "GoodCount"
+            readNodes[++index] = new OpcReadNode("ns=2;s={0}/GoodCount", deviceId);
+
+            // Read node for "BadCount"
+            readNodes[++index] = new OpcReadNode("ns=2;s={0}/BadCount", deviceId);
+
+            // Read node for "DeviceError"
+            readNodes[++index] = new OpcReadNode("ns=2;s={0}/DeviceError", deviceId);
+        }
+    }
+
+    // Perform read operation for all devices
+    var job = client.ReadNodes(readNodes);
+
+    // Iterate through the result and print the values
     foreach (var item in job)
     {
-        Console.WriteLine(item.Value);
+        Console.WriteLine($"Device: {item.NodeId}, Value: {item.Value}");
     }
 }
