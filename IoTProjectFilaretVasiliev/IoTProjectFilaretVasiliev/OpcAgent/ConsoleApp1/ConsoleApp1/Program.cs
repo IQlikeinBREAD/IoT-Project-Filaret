d12e0c9006904e55ef6a610371d2f3b653bb6ef7
IQlikeinBREAD <118021713+IQlikeinBREAD@users.noreply.github.com>
2023-12-20 16:07:13 +0100

    Add files via upload

---
+++ IoTProjectFilaretVasiliev/IoTProjectFilaretVasiliev/OpcAgent/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using Device;
+using Microsoft.Azure.Devices.Client;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static string deviceConnectionString = "HostName=IoT-uni-filaret6.azure-devices.net;DeviceId=Test-Device;SharedAccessKey=su85C/QkrwIfSsc+j1EX0KRGY/tD4VJcNAIoTAnvDlk=";
+        static async Task Main(string[] args)
+        {
+            using var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Mqtt);
+            await deviceClient.OpenAsync();
+
+            var device = new VirtualDevice(deviceClient);
+            await device.InitializeHandlers();
+            await device.UpdateTwinAsync();
+
+            Console.WriteLine($"Connection success!");
+
+            await device.SendMessages(10, 1000);
+
+            Console.WriteLine("Finished! Press Enter to close...");
+            Console.ReadLine();
+
+        }
+    }
+}
