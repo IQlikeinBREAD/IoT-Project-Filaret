d86c303f781e1cbcfdacd2568c144c923f659384
IQlikeinBREAD <118021713+IQlikeinBREAD@users.noreply.github.com>
2023-12-23 13:35:33 +0100

    main.py

--- Projekt/main.py
+++ Projekt/main.py
@@ -1,10 +1,13 @@
 import asyncio
-import json
 from asyncua import Client
 from azure.iot.device import IoTHubDeviceClient
-from Device import Device
+import json
+from Device import Device  # Переконайтеся, що клас Device належить модулю Device
 
-# Клас Device, який ви описали
+async def send_to_iot(device_client, device):
+    data = device.data()
+    message = json.dumps(data)
+    device_client.send_message(message)
 
 async def main():
     client = Client("opc.tcp://localhost:4840/")
@@ -13,33 +16,22 @@ async def main():
     CONNECTION_STRING = "HostName=IoT-uni-filaret6.azure-devices.net;DeviceId=Test-Device;SharedAccessKey=su85C/QkrwIfSsc+j1EX0KRGY/tD4VJcNAIoTAnvDlk="
     string_for_message = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
 
-    device_list = await client.get_objects_node().get_children()
-    device_list = device_list[1:]
+    while True:
+        device_list = await client.get_objects_node().get_children()
+        device_list = device_list[1:]
 
-    devices = []
+        devices = []
 
-    for device_node in device_list:
-        d = Device(client, device_node)
-        await d.wpisywanie()
+        for device_node in device_list:
+            d = Device(client, device_node)
+            await d.wpisywanie_danych()
+            devices.append(d)
         
-        # Отримання даних з пристрою в форматі, який можна відправити в Azure IoT Hub
-        device_info = {
-            "production_status": d.production_status,
-            "work_order_id": d.work_order_id,
-            "production_rate": d.production_rate,
-            "good_count": d.good_count,
-            "bad_count": d.bad_count,
-            "temperature": d.temperature
-            # Додайте інші дані, які ви хочете відправити
-        }
-
-        # Перетворення словника в JSON-подібний об'єкт
-        message_body = json.dumps(device_info)
-
-        # Відправлення повідомлення
-        string_for_message.send_message(message_body)
-
-    await client.disconnect()
+        for device in devices:
+            device_error =  device.device_errors()
+            print(device.device_errors())
+            if device_error != [0, 0, 0, 0]:
+                await send_to_iot(string_for_message, device)
 
 if __name__ == '__main__':
     asyncio.run(main())
