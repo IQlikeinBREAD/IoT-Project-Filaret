364213aa59da507b413d0d10a4fc3e8ae0ec02aa
IQlikeinBREAD <118021713+IQlikeinBREAD@users.noreply.github.com>
2024-02-13 14:41:58 +0100

    Update Device.py

--- Projekt/Device.py
+++ Projekt/Device.py
@@ -44,4 +44,7 @@ class Device:
     async def set_prod_rate(self, value=10):
         await self.client.set_values([self.client.get_node(f"{self.node}/ProductionRate")],
                                 [ua.DataValue(ua.Variant(int(self.production_rate - value), ua.VariantType.Int32))])
-        
+    
+    async def emergency_stop(self):
+        EmSt = self.client.get_node(f"{self.node}/EmergencyStop")
+        await self.node.call_method(EmSt)
