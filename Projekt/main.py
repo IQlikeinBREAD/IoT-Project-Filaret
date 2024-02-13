f0763d5e101b77bd1caeb58de07578eb84e7d9e1
IQlikeinBREAD <118021713+IQlikeinBREAD@users.noreply.github.com>
2024-02-13 14:06:18 +0100

    main.py

--- Projekt/main.py
+++ Projekt/main.py
@@ -2,21 +2,26 @@ import asyncio
 from asyncua import Client
 from azure.iot.device import IoTHubDeviceClient
 import json
-from Device import Device  # Переконайтеся, що клас Device належить модулю Device
+from Device import Device 
 
 async def send_to_iot(device_client, device):
     data = device.data()
     message = json.dumps(data)
     device_client.send_message(message)
 
+async def rep_twin(device_client,device):
+    reported_properties = {"Device" + str(machine.node)[-1]:}
+
 async def main():
     client = Client("opc.tcp://localhost:4840/")
     await client.connect()
 
     CONNECTION_STRING = "HostName=IoT-uni-filaret6.azure-devices.net;DeviceId=Test-Device;SharedAccessKey=su85C/QkrwIfSsc+j1EX0KRGY/tD4VJcNAIoTAnvDlk="
-    string_for_message = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
+    iot_client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
+    iot_client.connect()
 
     while True:
+        #Pobieramy dane
         device_list = await client.get_objects_node().get_children()
         device_list = device_list[1:]
 
@@ -27,11 +32,14 @@ async def main():
             await d.wpisywanie_danych()
             devices.append(d)
         
+        #Errors
         for device in devices:
-            device_error =  device.device_errors()
-            print(device.device_errors())
-            if device_error != [0, 0, 0, 0]:
-                await send_to_iot(string_for_message, device)
+            if device.get_errors() != [0,0,0,0]:
+                await send_to_iot(iot_client, device)
+    
+    await client.disconnect()
+    iot_client.disconnect()
+
 
 if __name__ == '__main__':
     asyncio.run(main())
