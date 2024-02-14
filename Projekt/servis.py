fd3fde87b466be3cab5533f2aa9926c3de551b97
IQlikeinBREAD <118021713+IQlikeinBREAD@users.noreply.github.com>
2024-02-14 15:16:55 +0100

    Update servis.py

--- Projekt/servis.py
+++ Projekt/servis.py
@@ -60,3 +60,60 @@ async def run_emergency_stop(manager, dev_name):
 
 async def run_res_err_status(manager, dev_name):
     await run_device_method(manager, "reset_err_status", dev_name)
+
+async def read_blobs(manager, device_id, connection_str, date_err, date_kpi):
+    blob_container_names = ['device-err', 'temperature-info', 'kpi-production']
+
+    blob_service_client = BlobServiceClient.from_connection_string(connection_str)
+
+    ret_date_err = date_err
+    ret_date_kpi = date_kpi
+
+    for blob_container_name in blob_container_names:
+        try:
+            container_client = blob_service_client.get_container_client(blob_container_name)
+
+            lst_blob_json = []
+
+            for blob in container_client.list_blobs():
+                data = container_client.download_blob(blob).readall()
+                data = data.decode("utf-8")
+                lst_blob_json.append(data.split('\r\n'))
+
+            lst_dic_blob = []
+
+            for i in range(len(lst_blob_json)):
+                for j in range(len(lst_blob_json[i])):
+                    lst_dic_blob.append(json.loads(lst_blob_json[i][j]))
+
+            print(f"{blob_container_name.upper()} JSON: " + str(lst_blob_json))
+            print(f"{blob_container_name.upper()} DIC: " + str(lst_dic_blob))
+
+            if blob_container_name == 'device-err':
+                for i in range(len(lst_dic_blob)):
+                    if lst_dic_blob[i]["windowEndTime"] > date_err:
+                        ret_date_err = lst_dic_blob[i]["windowEndTime"]
+                        device_name = lst_dic_blob[i]["DeviceName"]
+                        await run_emergency_stop(manager, device_name)
+
+            elif blob_container_name == 'kpi-production':
+                for i in range(len(lst_dic_blob)):
+                    if lst_dic_blob[i]["windEndTime"] > date_kpi:
+                        ret_date_kpi = lst_dic_blob[i]["windEndTime"]
+
+                        if lst_dic_blob[i]["KPI"] < 90:
+                            print("---------kpi------------")
+                            twin = manager.get_twin(device_id)
+                            des = twin.properties.desired
+
+                            dev_name = "Device" + str(lst_dic_blob[i]["DeviceName"])[-1]
+                            prod_rate = des[dev_name]["ProductionRate"] - 10
+
+                            update_tw = {dev_name: {"ProductionRate": prod_rate}}
+
+                            twin_patch = Twin(properties=TwinProperties(desired=update_tw))
+                            twin = manager.update_twin(device_id, twin_patch, twin.etag)
+        except:
+            pass
+
+    return ret_date_err, ret_date_kpi
