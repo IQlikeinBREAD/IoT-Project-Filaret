d4a96f74d3d8b8f297f701cec9e79a12c38fcf6c
IQlikeinBREAD <118021713+IQlikeinBREAD@users.noreply.github.com>
2024-02-14 15:12:45 +0100

    Update servis.py

--- Projekt/servis.py
+++ Projekt/servis.py
@@ -24,3 +24,14 @@ async def twin_desired(manager_client, device_id, reported):
     twin = manager_client.get_twin(device_id)
     twin_patch = Twin(properties=TwinProperties(desired=desired_twin))
     twin = manager_client.update_twin(device_id, twin_patch, twin.etag)
+
+async def clear_desired_twin(manager, device_id):
+    twin = manager.get_twin(device_id)
+    des = twin.properties.desired
+    del des["$metadata"]
+    del des["$version"]
+    for key, value in des.items():
+        des[key] = None
+
+    twin_patch = Twin(properties=TwinProperties(desired=des))
+    twin = manager.update_twin(device_id, twin_patch, twin.etag)
